5af82833170d3a94d62277d5facecf3cdd531dd3
Americo <ame.zuz@gmail.com>
2018-05-06 18:18:52 -0700

    Delete and depth

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -35,16 +35,92 @@ class BinarySearchTree
   end
 
   def delete(value)
+    return @root = nil if @root.value == value && childless?(@root)
+
+    to_delete = find(value)
+    left = to_delete.parent.left == to_delete
+
+    if childless?(to_delete)
+      if left
+        to_delete.parent.left = nil
+      else
+        to_delete.parent.right = nil
+      end
+    elsif two_children?(to_delete)
+      handle_both_kids(to_delete, left)
+    else
+      handle_children(to_delete, left)
+    end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+     return tree_node if tree_node.right.nil?
+
+     maximum(tree_node.right)
+  end
+
+  def childless?(node)
+    node.left.nil? && node.right.nil?
+  end
+
+  def two_children?(node)
+    node.left && node.right
+  end
+
+  def handle_children(node, left)
+    left_child = node.left
+    right_child = node.right
+
+    if left_child && !right_child
+      if left
+        node.parent.left = left_child
+      else
+        node.parent.right = left_child
+      end
+    elsif !left_child && right_child
+      if left
+        node.parent.left = right_child
+      else
+        node.parent.right = right_child
+      end
+    end
+  end
+
+  def handle_both_kids(node, left)
+    max_node = maximum(node.left)
+
+    max_node.left.parent = max_node.parent
+    max_node.parent.right = max_node.left
+
+    if left
+      node.parent.left = max_node
+    else
+      node.parent.right = max_node
+    end
+
+    max_node.parent = node.parent
   end
 
   def depth(tree_node = @root)
+    depth_charge = 0
+    return depth_charge if childless?(tree_node)
+
+    left_depth = tree_node.left ? depth(tree_node.left) + 1 : 0
+    right_depth = tree_node.right ? depth(tree_node.right) + 1 : 0
+
+    depth_charge = left_depth > right_depth ? left_depth : right_depth
+
+    depth_charge
   end
 
   def is_balanced?(tree_node = @root)
+    return true if childless?(tree_node)
+    if (depth(tree_node.right) - depth(tree_node.left)) < 2
+      is_balanced?(tree_node.left) && is_balanced?(tree_node.right)
+    else
+      false
+    end
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
@@ -58,12 +134,14 @@ class BinarySearchTree
     when 1
       if parent_node.right.nil?
         parent_node.right = BSTNode.new(value)
+        parent_node.right.parent = parent_node
       else
         find_spot(parent_node.right, value)
       end
     when -1, 0
       if parent_node.left.nil?
         parent_node.left = BSTNode.new(value)
+        parent_node.left.parent = parent_node
       else
         find_spot(parent_node.left, value)
       end
