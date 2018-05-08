dad9cd9049bf7360f7c5fabeba8e6ac06e94f297
Americo <ame.zuz@gmail.com>
2018-05-07 17:10:20 -0700

    Complete search for kth biggest node

--- lib/bst_practical.rb
+++ lib/bst_practical.rb
@@ -1,3 +1,15 @@
+require 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  traversed = reverse_traversal(tree_node)
+  traversed[k - 1]
+end
+
+def reverse_traversal(tree_node)
+  return [tree_node] if !tree_node.left && !tree_node.right
+
+  right = tree_node.right ? reverse_traversal(tree_node.right) : []
+  left = tree_node.left ? reverse_traversal(tree_node.left) : []
+
+  right + [tree_node] + left
 end
